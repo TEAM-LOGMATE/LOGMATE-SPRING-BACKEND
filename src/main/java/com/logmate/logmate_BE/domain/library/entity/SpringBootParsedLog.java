c71bfa5d2c8c10a5e10e539d3049705d1e2ebbcb
kangchanwook <kan0202@naver.com>
2025-04-12 13:59:18 +0900

    [Feat] SpringBoot Log 를 받기 위한 Entity 추가 및 기존 Message Entity 삭제

---
+++ src/main/java/com/logmate/logmate_BE/domain/library/entity/SpringBootParsedLog.java
@@ -0,0 +1,33 @@
+package com.logmate.logmate_BE.domain.library.entity;
+
+
+import jakarta.persistence.*;
+import java.time.LocalDateTime;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Builder
+public class SpringBootParsedLog {
+
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  private Long id;
+
+  private boolean isFormatCorrect;
+  private LocalDateTime timestamp;
+  private String level;
+  private String thread;
+  private String logger;
+
+  @Column(nullable = false)
+  private String message;
+
+  @Column(nullable = false)
+  private String userCode;
+}
